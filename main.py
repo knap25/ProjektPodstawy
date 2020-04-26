23e8a2862485a99bb64ffe8854f414cb562ea0f6
vdi-student <kasia.napieracz@gmail.com>
2020-04-26 12:54:26 +0200

    szkielet testu

---
+++ main.py
@@ -0,0 +1,16 @@
+import unittest
+from selenium import webdriver
+
+
+class MojPrzypadekTestowy(unittest.TestCase):
+    def setUp(self):
+        print("Przygotowanie testu")
+        self.driver = webdriver.Firefox()
+
+    def testSelenium(self):
+        print("test wlasciwy")
+    def tearDown(self):
+        print("sprzatanie po tescie")
+
+if __name__=='__main__':
+    unittest.main()
