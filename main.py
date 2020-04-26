5bc70f4ef57ac3ede9665216bbb54cb67d20864c
vdi-student <kasia.napieracz@gmail.com>
2020-04-26 14:43:36 +0200

    zadanie cz.1 po zmianach

--- main.py
+++ main.py
@@ -7,31 +7,20 @@ class MojPrzypadekTestowy(unittest.TestCase):
     def setUp(self):
         print("Przygotowanie testu")
         self.driver = webdriver.Firefox()
-    #zadanie pkt 1
         self.driver.get("https://www.sukienkimm.pl/")
-    #powiekszenie strony
         self.driver.maximize_window()
 
     def testSelenium(self):
-    #zeby nie trzebabylo pisac self.driver wpisuje driver=
         driver = self.driver
-        print("wylaczam alert")
-        al = driver.find_element_by_id('onesignal-popover-cancel-button')
+        print("Wylaczam alert")
+        al = driver.find_element_by_id("onesignal-popover-cancel-button")
         al.click()
-        print("klikam zaloguj sie")
+        print("Klikam zaloguj sie")
         zaloguj_btn = driver.find_element_by_xpath('//span[text()=" zaloguj się"]')
         zaloguj_btn.click()
-    #sleep tylko w celach debagowych
+        # Sleep tylko w celach debagowych
         sleep(2)
 
-
     def tearDown(self):
-        print("sprzatanie po tesciee")
-    #zamkniecie strony
+        print("sprzatanie po tescie")
         self.driver.quit()
-
-
-
-
-if __name__=='__main__':
-    unittest.main()
